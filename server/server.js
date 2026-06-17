e300dbbcee5c146ad527262202916a3cd5d243e3
Phoenix2816 <145472889+Phoenix2816@users.noreply.github.com>
2026-06-17 16:52:03 +0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -373,10 +373,6 @@ async function startServer() {
             }
         });
 
-        // START SERVER 
-        app.listen(process.env.PORT, () => {
-            console.log(`Server running on port ${process.env.PORT}`);
-        });
 
     } catch (err) {
         console.error("DB connection failed:", err);
@@ -384,3 +380,4 @@ async function startServer() {
 }
 
 startServer();
+module.exports = app;
